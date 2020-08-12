db679b9cfb8f6fa271fac4ded49e44ca74763dc2
Jon Hieb <hiebj@live.com>
2020-08-12 11:46:01 -0600

    Update GradeChart.js

--- GradeChart.js
+++ GradeChart.js
@@ -12,23 +12,25 @@ function GradeChart($window, grades) {
         $scope.animDelay = 200;
         $scope.grades = grades;
         processGrades();
-        $window.on('resize', processGrades);
+        angular.element($window).on('resize', processGrades);
 
         function processGrades() {
-            $scope.width = $element[0].offsetWidth;
-            var interiorWidth = $scope.width - $scope.margin.left - $scope.margin.right;
-            $scope.barHeight = 58;
-            var halfBar = $scope.barHeight / 2;
-            var interiorHeight = $scope.grades.length * ($scope.barHeight + 3);
-            $scope.height = interiorHeight + $scope.margin.top + $scope.margin.bottom;
-            $scope.gridTickSize = interiorHeight + 3;
+            var elementWidth = $element[0].offsetWidth;
+            if (elementWidth > 0) {
+                $scope.width = $element[0].offsetWidth;
+                var interiorWidth = $scope.width - $scope.margin.left - $scope.margin.right;
+                $scope.barHeight = 58;
+                var halfBar = $scope.barHeight / 2;
+                var interiorHeight = $scope.grades.length * ($scope.barHeight + 3);
+                $scope.height = interiorHeight + $scope.margin.top + $scope.margin.bottom;
+                $scope.gridTickSize = interiorHeight + 3;
 
-            $scope.xScale = d3.scale.linear()
-                .domain([ 0, 100 ])
-                .range([ 0, interiorWidth ]);
-            $scope.yScale = d3.scale.linear()
-                .domain([ 0, $scope.grades.length - 1 ])
-                .range([ halfBar + 3, interiorHeight - halfBar ]);
+                $scope.xScale = d3.scale.linear()
+                    .domain([ 0, 100 ])
+                    .range([ 0, interiorWidth ]);
+                $scope.yScale = d3.scale.linear()
+                    .domain([ 0, $scope.grades.length - 1 ])
+                    .range([ halfBar + 3, interiorHeight - halfBar ]);
         }
     }
 
